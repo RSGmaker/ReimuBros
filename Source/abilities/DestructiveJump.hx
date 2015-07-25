fe342a9193132cb8c33e23b8738066bec09f8dce
RSGmaker <rsraygamer@gmail.com>
2015-07-25 15:13:39 -0600

    added flandre's ability

---
+++ Source/abilities/DestructiveJump.hx
@@ -0,0 +1,49 @@
+package abilities;
+
+/**
+ * ...
+ * @author RSGmaker
+ */
+class DestructiveJump extends PlayerAbility
+{
+	public function new(player:Player) 
+	{
+		super(player);
+		
+	}
+	override public function init() 
+	{
+		super.init();
+		player.maxcooldown = 800;
+		player.warncooldown = true;
+	}
+	override public function onframe() 
+	{
+		super.onframe();
+		player.cooldown--;
+		if (player.Vspeed >= 0)
+		{
+			player.superpower = false;
+		}
+	}
+	override public function onuse() 
+	{
+		super.onuse();
+		if (player.isme && player.cooldown <= 0 && player.ground!=null)
+		{
+			player.cooldown = player.maxcooldown;
+			player.superpower = true;
+			player.Vspeed = -30;
+		}
+	}
+	override public function onheadbonk() 
+	{
+		super.onheadbonk();
+		if (player.superpower)
+		{
+			player.superpower = false;
+			player.cooldown = player.maxcooldown;
+			game.SendEvent("Earthquake", player.headbonk.y);
+		}
+	}
+}
