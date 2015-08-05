aebbf02a98b4f5819d2e3d944675a3bb190aee16
RSGmaker <rsraygamer@gmail.com>
2015-08-05 17:04:57 -0600

    changed poisoncloud visual

--- Source/PoisonCloud.hx
+++ Source/PoisonCloud.hx
@@ -8,6 +8,7 @@ import openfl.geom.Matrix;
 import openfl.geom.Point;
 import openfl.display.BlendMode;
 import openfl.display.GradientType;
+import openfl.display.Graphics;
 /**
  * ...
  * @author RSGmaker
@@ -15,46 +16,60 @@ import openfl.display.GradientType;
 class PoisonCloud extends Sprite
 {
 	public var msk:Shape;
+	public var msk2:Shape;
 	public var texture1:Bitmap;
 	public var texture2:Bitmap;
 	public var texture1movement:Point;
 	public var texture2movement:Point;
-	public function new() 
+	public var block:Block;
+	public var left:Bool;
+	public var right:Bool;
+	public var R:Float;
+	private var filterArr:Array<flash.filters.BitmapFilter>;
+	public function new(block:Block) 
 	{
 		super();
+		this.block = block;
 		var bd = Main._this.AL.GetAnimation("poisoncloud")[0];
 		texture1 = new Bitmap(bd);
 		texture2 = new Bitmap(bd);
 		msk = new Shape();
-		var R = 32;
+		R = 32;
 		var mtx:Matrix = new Matrix();
-		mtx.createGradientBox(R+R,R+R,0,-R,-R);
-		msk.graphics.beginGradientFill(GradientType.RADIAL, [0xDDDDDD, 0xFF6F22], [1,0], [0,255], mtx);
+		//mtx.createGradientBox(R+R,R+R,0,-R,-R);
+		/*msk.graphics.beginGradientFill(GradientType.RADIAL, [0xDDDDDD, 0xFF6F22], [1,0], [0,255], mtx);
 		msk.graphics.drawCircle(64, 64, R);
-		msk.graphics.endFill();
+		msk.graphics.endFill();*/
 		//msk.cacheAsBitmap = true;
 		msk.visible = false;
 		addChild(msk);
 		texture1.mask = msk;
-		msk = new Shape();
-		msk.graphics.beginGradientFill(GradientType.RADIAL, [0xDDDDDD, 0xFF6F22], [1,0], [0,255], mtx);
-		msk.graphics.drawCircle(64, 64, R);
-		msk.graphics.endFill();
+		msk2 = new Shape();
+		/*msk2.graphics.beginGradientFill(GradientType.RADIAL, [0xDDDDDD, 0xFF6F22], [1,0], [0,255], mtx);
+		msk2.graphics.drawCircle(64, 64, R);
+		msk2.graphics.endFill();*/
 		//msk.cacheAsBitmap = true;
-		texture2.mask = msk;
-		texture1.blendMode = BlendMode.LIGHTEN;
+		texture2.mask = msk2;
+		//texture1.blendMode = BlendMode.LIGHTEN;
 		texture2.blendMode = BlendMode.LIGHTEN;
+		
 		//texture1.cacheAsBitmap = true;
 		//texture2.cacheAsBitmap = true;
 		reset();
-		msk.visible = false;
-		addChild(msk);
+		msk2.visible = false;
+		addChild(msk2);
 		addChild(texture1);
 		addChild(texture2);
 		
 		texture1.alpha = 0.5;
 		texture2.alpha = 0.5;
-		
+		redomasking();
+		/*
+		var blur = new flash.filters.BlurFilter(8, 8, 1);
+		filterArr = new Array<flash.filters.BitmapFilter>();
+		filterArr[0] = blur;
+		filters = filterArr;
+		*/
 	}
 	
 	public function reset()
@@ -88,8 +103,115 @@ class PoisonCloud extends Sprite
 			texture.y -= 64;
 		}
 	}
+	public function _redomasking(mask:Shape)
+	{
+		mask.graphics.clear();
+		if (!left || !right)
+		{
+		//mask.graphics.beginGradientFill(GradientType.RADIAL, [0xDDDDDD, 0xFF6F22], [1,0], [0,255], mtx);
+		mask.graphics.beginFill(0xFFFFFF);
+		if (!left && !right)
+		{
+			mask.graphics.drawCircle(64, 64, R);
+		}
+		else
+		{
+			if (!left)
+			{
+				halfCircle(mask.graphics, 64, 64, R,true);
+			}
+			else
+			{
+				halfCircle(mask.graphics, 64, 64, R,false);
+			}
+		}
+		mask.graphics.endFill();
+		}
+		if (left && right)
+		{
+			mask.graphics.beginFill(0xFFFFFF);
+			//mask.graphics.drawRect(R+16, R, R, R + R);
+			mask.graphics.drawRect(R, R, R+32, R + R);
+			mask.graphics.endFill();
+		}
+		else
+		{
+		if (left)
+		{
+			mask.graphics.beginFill(0xFFFFFF);
+			//mask.graphics.drawRect(R+16, R, 16, R + R);
+			mask.graphics.drawRect(R, R, 32, R + R);
+			mask.graphics.endFill();
+		}
+		if (right)
+		{
+			mask.graphics.beginFill(0xFFFFFF);
+			//mask.graphics.drawRect(R+R, R, 16, R + R);
+			mask.graphics.drawRect(R+R, R, 32, R + R);
+			mask.graphics.endFill();
+		}
+		}
+	}
+	
+function halfCircle(g:Graphics, x:Float,y:Float,r:Float,left:Bool) {
+    var c1:Float=r * (1.41421356237 - 1);
+    var c2:Float = r * 1.41421356237 / 2;
+	g.moveTo(x, y + r);
+	if (!left)
+	{
+		g.curveTo(x+c1,y+r,x+c2,y+c2);
+		g.curveTo(x+r,y+c1,x+r,y);
+		g.curveTo(x+r,y-c1,x+c2,y-c2);
+		g.curveTo(x + c1, y - r, x, y - r);
+	}
+	else
+	{
+		g.curveTo(x-c1,y+r,x-c2,y+c2);
+		g.curveTo(x-r,y+c1,x-r,y);
+		g.curveTo(x-r,y-c1,x-c2,y-c2);
+		g.curveTo(x - c1, y - r, x, y - r);
+	}
+    /*g.moveTo(x+r,y);
+    g.curveTo(x+r,y+c1,x+c2,y+c2);
+    g.curveTo(x+c1,y+r,x,y+r);
+    g.curveTo(x-c1,y+r,x-c2,y+c2);
+    g.curveTo(x-r,y+c1,x-r,y);*/
+    // comment in for full circle
+    /*g.curveTo(x-r,y-c1,x-c2,y-c2);
+    g.curveTo(x-c1,y-r,x,y-r);
+    g.curveTo(x+c1,y-r,x+c2,y-c2);
+    g.curveTo(x+r,y-c1,x+r,y);*/
+};
+
+	public function redomasking()
+	{
+		_redomasking(msk);
+		_redomasking(msk2);
+		if (left && right)
+		{
+			//texture1.alpha = 0.75;
+			//texture2.alpha = 0.75;
+			texture1.alpha = 0.3525;
+			texture2.alpha = 0.3525;
+		}
+		else
+		{
+			texture1.alpha = 0.5;
+			texture2.alpha = 0.5;
+			//texture1.alpha = 0.3525;
+			//texture2.alpha = 0.3525;
+		}
+	}
 	/*override */public function update():Void 
 	{
+		var L = (block.LeftBlock != null && block.LeftBlock.solid);
+		var R = (block.RightBlock != null && block.RightBlock.solid);
+		if (L != left || R != right)
+		{
+			left = L;
+			right = R;
+			redomasking();
+		}
 		//super.update();
 		texture1.x += texture1movement.x;
 		texture1.y += texture1movement.y;
