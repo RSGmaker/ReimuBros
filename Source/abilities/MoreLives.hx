80ca1d1baf2678fff58b6597cec8e50661564a31
RSGmaker <rsraygamer@gmail.com>
2015-08-04 23:26:29 -0600

    removed extra lives code

--- Source/abilities/MoreLives.hx
+++ Source/abilities/MoreLives.hx
@@ -10,22 +10,7 @@ class MoreLives extends PlayerAbility
 	public function new(player:Player) 
 	{
 		super(player);
-<<<<<<< HEAD
-		description = "Starts with double the lives";
-		toggle = false;
-=======
 		description = "Each life is worth two";
->>>>>>> origin/master
-	}
-	override public function init() 
-	{
-		super.init();
-		player.lives += 3;
-	}
-	override public function oncontinue() 
-	{
-		super.oncontinue();
-		player.lives += 3;
 	}
 	override public function onloselife()
 	{
