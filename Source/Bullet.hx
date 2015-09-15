4d06bf4e7fa94ad773669580a7dddb5defbc9e92
RSGmaker <rsraygamer@gmail.com>
2015-09-15 17:26:08 -0600

    made enemy bullets work properly again

--- Source/Bullet.hx
+++ Source/Bullet.hx
@@ -10,14 +10,15 @@ class Bullet extends Entity
 	public var gravX:Float = 0;
 	public var gravY:Float = 0;
 	public var started:Bool;
+	public var HP:Int = 0;
 	public function new() 
 	{
 		super("bullet");
 		
-		dangerous = true;
 		type = "Bullet";
 		//if we kept track of these things would probly get messy.
 		UID = -1;
+		dangerous = true;
 	}
 	public override function update()
 	{
@@ -36,6 +37,15 @@ class Bullet extends Entity
 		x += Hspeed;
 		y += Vspeed;
 		var ok = true;
+		if (HP > 0)
+		{
+			HP--;
+			if (HP < 1)
+			{
+				alive = false;
+				killed = true;
+			}
+		}
 		if ((x < -width && gravX<=0 && Hspeed<=0) || (y < -height && gravY<=0 && Vspeed<=0) || (x>832 && gravX>=0 && Hspeed>=0) || (y>832 && gravY>=0 && Vspeed>=0))
 		{
 			alive = false;
